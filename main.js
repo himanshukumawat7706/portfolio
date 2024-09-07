a96a0b3393e8736d7cfab7424cdc26a9bc58e2db
himanshukumawat7706 <hk2196@srmist.edu.in>
2024-09-07 22:10:38 +0530

    Add files via upload
    this is my first portfolio website

---
+++ main.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="himanshuk7706@gmail.com" method="post" enctype="text/plain">
+        <label>Your Message:</label>
+        <br>
+        <textarea name="Message " rows="7" cols="50"></textarea>
+        <br>
+        <label>Choose File:</label>
+        <input type="file" name="File ">
+        <br>
+        <label>Your Name:</label>
+        <input type="text" name="Name " value="Type your full name">
+        <br>
+        <label>Your Email:</label>
+        <input type="email" name="Email ">
+        <br>
+        <label>Date of Birth:</label>
+        <input type="date" name="Date of Birth ">
+        <br>
+        <label>Your Password:</label>
+        <input type="password" name="Password ">
+        <br>
+        <input type="submit" name="">
+    </form>
+</body>
+</html>
